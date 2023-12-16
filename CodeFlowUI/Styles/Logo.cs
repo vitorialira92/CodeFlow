d161798caa68f801555d254633410848b9f32c09
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-16 16:29:49 -0300

    picture box com o logo

---
+++ CodeFlowUI/Styles/Logo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowUI.Styles
+{
+    internal class Logo : PictureBox
+    {
+        internal Logo()
+        {
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Image = Image.FromFile(@"Resources\logo-nome.png");
+        }
+    }
+}
