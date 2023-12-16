8f6aa10709f549f16135fe533c29d21d828ffb05
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-16 16:28:03 -0300

    adicionando mais cores fixas

--- CodeFlowUI/Styles/Colors.cs
+++ CodeFlowUI/Styles/Colors.cs
@@ -12,5 +12,7 @@ namespace CodeFlowUI.Styles
     {
         public static readonly Color CallToActionButton = ColorTranslator.FromHtml("#B1C9FC");
         public static readonly Color CallToActionText = ColorTranslator.FromHtml("#1F3461");
+        public static readonly Color TextBox = ColorTranslator.FromHtml("#F8FBFF");
+        public static readonly Color StrokeTextBox = ColorTranslator.FromHtml("#D1D0D0");
     }
 }
