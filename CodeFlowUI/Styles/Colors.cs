c45bcd1461f61cd6a7a94f587a5e20598205fa28
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-24 16:04:42 -0300

    feat: more colors

--- CodeFlowUI/Styles/Colors.cs
+++ CodeFlowUI/Styles/Colors.cs
@@ -10,9 +10,21 @@ namespace CodeFlowUI.Styles
 {
     internal static class Colors
     {
+        //components
         public static readonly Color CallToActionButton = ColorTranslator.FromHtml("#B1C9FC");
         public static readonly Color CallToActionText = ColorTranslator.FromHtml("#1F3461");
         public static readonly Color TextBox = ColorTranslator.FromHtml("#F8FBFF");
         public static readonly Color StrokeTextBox = ColorTranslator.FromHtml("#D1D0D0");
+        public static readonly Color StrokeContainer = ColorTranslator.FromHtml("#B5AFAF");
+        public static readonly Color DarkBlue = ColorTranslator.FromHtml("#21005D");
+
+        //project status
+        public static readonly Color OpenProject = ColorTranslator.FromHtml("#FAFF00");
+        public static readonly Color OngoingProject = ColorTranslator.FromHtml("#05FF00");
+        public static readonly Color LateProject = ColorTranslator.FromHtml("#FF0000");
+        public static readonly Color CanceledProject = ColorTranslator.FromHtml("#FF7A00");
+        public static readonly Color DoneProject = ColorTranslator.FromHtml("#026C00");
+        public static readonly Color ProjectCardBackgroundColor = ColorTranslator.FromHtml("#F6FBFF");
+
     }
 }
