215b5a23b6250fb05f49559c2ce5fa8ac35384a6
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-25 16:38:39 -0300

    fix: correct constructor

---
+++ CodeFlowBackend/Model/User/Developer.cs
@@ -0,0 +1,22 @@
+﻿using CodeFlowBackend.Model.User.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Model.User
+{
+    internal class Developer : User
+    {
+        public Developer(string username, string firstName, string lastName, string email, string password, ExperienceLevelDeveloper experienceLevel) 
+            : base(username, firstName, lastName, email, password, UserRole.Developer)
+        {
+            this.ExperienceLevel = experienceLevel;
+        }
+
+        public int DeveloperId { get; private set; }
+        public ExperienceLevelDeveloper ExperienceLevel { get; private set; }
+
+    }
+}
