045e16c7bf55c36f008fc22b1649757b842fa7d2
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-25 16:34:58 -0300

    feat: new methods

---
+++ CodeFlowBackend/Services/UserService.cs
@@ -0,0 +1,72 @@
+﻿using CodeFlowBackend.DTO;
+using CodeFlowBackend.Model.User;
+using CodeFlowBackend.Model.User.Enum;
+using CodeFlowBackend.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Services
+{
+    public static class UserService
+    {
+        private static long AddDeveloper(RegisterDTO newUser)
+        {
+            Developer developer = new Developer(newUser.Username, newUser.FirstName, 
+                newUser.LastName, newUser.Email, newUser.Password, (ExperienceLevelDeveloper) newUser.SpecificToUserRole );
+
+            if (UserRepository.AddDeveloper(developer))
+                return UserRepository.GetUserIdByEmail(newUser.Email);
+            
+             return 0;
+        }
+
+        private static long AddTechLeader(RegisterDTO newUser)
+        {
+            TechLeader techLeader = new TechLeader((Specializations)newUser.SpecificToUserRole, newUser.Username, newUser.FirstName,
+                newUser.LastName, newUser.Email, newUser.Password);
+
+            if (UserRepository.AddTechLeader(techLeader))
+                return UserRepository.GetUserIdByEmail(newUser.Email);
+
+            return 0;
+        }
+
+        public static long Login(string username, string password)
+        {
+            return UserRepository.Login(username, password);
+        }
+
+        public static UserBasicInfoDTO GetUserBasicInfo(long userId)
+        {
+            User user = UserRepository.GetUserById(userId);
+
+            return new UserBasicInfoDTO(user.Id, user.FirstName, user.Role);
+        }
+
+        public static bool IsUsernameAvailable(string username)
+        {
+            return UserRepository.GetIdByUsername(username) == 0;
+        }
+
+        public static long AddUser(RegisterDTO newUser)
+        {
+            long newUserId = 0;
+
+            if (newUser.IsTechLeader)
+                newUserId = AddTechLeader(newUser);
+            else
+                newUserId = AddDeveloper(newUser);
+
+            return newUserId;
+        }
+
+        public static bool IsEmailAvailable(string email)
+        {
+            return UserRepository.GetUserIdByEmail(email) == 0;
+        }
+    }
+
+}
