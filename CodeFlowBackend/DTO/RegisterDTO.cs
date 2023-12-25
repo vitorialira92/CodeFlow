41e6c50339f1ba19ea0023c02fb95573f91feb97
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-25 16:32:39 -0300

    feat: register user dto

---
+++ CodeFlowBackend/DTO/RegisterDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.DTO
+{
+    public class RegisterDTO
+    {
+        public RegisterDTO(string firstName, string lastName, string email, string username, string password, bool isTechLeader, int specificToUserRole)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email.ToLower();
+            Username = username.ToLower();
+            Password = password;
+            IsTechLeader = isTechLeader;
+            SpecificToUserRole = specificToUserRole;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public bool IsTechLeader { get; private set; }
+        public int SpecificToUserRole { get; private set; }
+
+       
+    }
+}
