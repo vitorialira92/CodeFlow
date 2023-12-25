1c4b1d4662c28844de9c6b9d84aa855e004d7529
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-25 16:33:12 -0300

    fix: a new constructor

---
+++ CodeFlowBackend/Model/User/TechLeader.cs
@@ -0,0 +1,31 @@
+﻿using CodeFlowBackend.Model.User.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Model.User
+{
+    internal class TechLeader : User
+    {
+        public int TechLeaderId { get; private set; }
+        public Specializations Specialization { get; private set; }
+
+        public TechLeader(int userId, int techLeaderId, Specializations specialization, 
+            string username, string firstName, string lastName, string email, string password) : base(userId, username, firstName, lastName, 
+                email, password, UserRole.TechLeader)
+        {
+            TechLeaderId = techLeaderId;
+            Specialization = specialization;
+        }
+
+        public TechLeader(Specializations specialization, 
+            string username, string firstName, string lastName,string email, string password) : base(username, firstName, lastName,
+                email, password, UserRole.TechLeader)
+        {
+            Specialization = specialization;
+        }
+
+    }
+}
