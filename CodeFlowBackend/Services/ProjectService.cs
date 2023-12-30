e3d9a57caaf90c57452b21f63a1755d29487ef50
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-30 16:18:55 -0300

    feat: project service

---
+++ CodeFlowBackend/Services/ProjectService.cs
@@ -0,0 +1,29 @@
+﻿using CodeFlowBackend.DTO;
+using CodeFlowBackend.Model.Project;
+using CodeFlowBackend.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Services
+{
+    public static class ProjectService
+    {
+        public static List<ProjectBasicInfoDTO> GetAllProjectsBasicInfoByUserId(long userId)
+        {
+            List<Project> projects = ProjectRepository.GetAllActiveProjectsByUserId(userId);
+
+            List<ProjectBasicInfoDTO> projectBasicInfoDTOs = new List<ProjectBasicInfoDTO>();
+
+            foreach(var project in projects)
+            {
+                projectBasicInfoDTOs.Add(new ProjectBasicInfoDTO(project.Id, project.Name, project.Description, project.Status, project.DueDate));
+            }
+
+            return projectBasicInfoDTOs;
+        }
+
+    }
+}
