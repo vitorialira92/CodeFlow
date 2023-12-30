ba26eb8ff9e1a8a9388732abc9899fe0d7a18a4e
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-30 16:17:55 -0300

    feat: dto for project page

---
+++ CodeFlowBackend/DTO/ProjectPageDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.DTO
+{
+    public class ProjectPageDTO
+    {
+        public long ProjectId { get; private set; }
+        public long UserId { get; private set; }
+        public bool IsUserTechLeader {  get; private set; }
+
+        public ProjectPageDTO(long projectId, long userId, bool isUserTechLeader)
+        {
+            ProjectId = projectId;
+            UserId = userId;
+            IsUserTechLeader = isUserTechLeader;
+        }
+    }
+}
