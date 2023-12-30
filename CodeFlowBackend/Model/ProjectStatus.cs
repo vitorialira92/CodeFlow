def22577013413c1b9b582d54f24bdccd16358e5
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-30 16:17:30 -0300

    feat: project status enum

---
+++ CodeFlowBackend/Model/ProjectStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Model
+{
+    public enum ProjectStatus
+    {
+        Open = 1,
+        OnGoing = 2,
+        Canceled = 3,
+        Done = 4,
+        Late = 5
+    }
+}
