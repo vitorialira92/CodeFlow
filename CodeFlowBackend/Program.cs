801ea4b5e5f992efc9ee938a0e53e0ade59cda35
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-31 14:57:17 -0300

    fix: removing unnecessary code

--- CodeFlowBackend/Program.cs
+++ CodeFlowBackend/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace CodeFlowBackend
+﻿using CodeFlowBackend.Model.User;
+using CodeFlowBackend.Repositories;
+using CodeFlowBackend.Services;
+
+namespace CodeFlowBackend
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+           
         }
     }
 }
