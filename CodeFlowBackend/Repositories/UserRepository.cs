34466fb37e61e4050c9efc6e2e3288eec70f85c3
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-31 14:55:08 -0300

    feat: add the following methods: get user by id, get user's project count by id, get user's done tasks count by id and update user/tech leader/developer

--- CodeFlowBackend/Repositories/UserRepository.cs
+++ CodeFlowBackend/Repositories/UserRepository.cs
@@ -16,7 +16,6 @@ namespace CodeFlowBackend.Repositories
 {
     internal class UserRepository : DBConnection
     {
-        private static UserRepository _instance;
         private UserRepository() { }
 
         private static bool AddUser(User user)
@@ -38,7 +37,7 @@ namespace CodeFlowBackend.Repositories
                 _command.Parameters.AddWithValue("@firstName", user.FirstName);
                 _command.Parameters.AddWithValue("@lastName", user.LastName);
                 _command.Parameters.AddWithValue("@dateJoined", user.DateJoined);
-                _command.Parameters.AddWithValue("@role", (int) user.Role);
+                _command.Parameters.AddWithValue("@role", (int)user.Role);
                 _command.Parameters.AddWithValue("@password", password_salt.password);
 
                 query = "INSERT INTO user_salt (user_id, salt) VALUES (@user_id, @salt);";
@@ -87,7 +86,7 @@ namespace CodeFlowBackend.Repositories
 
             }
             finally { Close(); }
-            
+
         }
 
         public static bool AddDeveloper(Developer user)
@@ -105,7 +104,7 @@ namespace CodeFlowBackend.Repositories
 
                     _command = new SQLiteCommand(query, _connection);
 
-                    _command.Parameters.AddWithValue("@experience_level", (int) user.ExperienceLevel);
+                    _command.Parameters.AddWithValue("@experience_level", (int)user.ExperienceLevel);
                     _command.Parameters.AddWithValue("@user_id", id);
 
 
@@ -166,7 +165,7 @@ namespace CodeFlowBackend.Repositories
                 Close();
             }
         }
-           
+
 
         internal static (long? id, bool? isTechLeader) Login(string username, string password)
         {
@@ -217,57 +216,137 @@ namespace CodeFlowBackend.Repositories
             }
         }
 
-        internal static User GetUserById(long userId)
+        internal static Developer GetDeveloperById(long userId)
         {
-            User user = null;
-            Open();
+            try
+            {
+                Open();
 
-            string query = @"SELECT * 
+                string query = @"SELECT id, username, email, first_name, last_name, date_joined, role
                  FROM user
                  WHERE id = @id;";
-            _command = new SQLiteCommand(query, _connection);
-            _command.Parameters.AddWithValue("@id", userId);
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@id", userId);
 
-            var reader = _command.ExecuteReader();
+                var reader = _command.ExecuteReader();
 
-            if (reader.Read())
-            {
-                long id = (long)reader["id"];
-                string username = reader["username"].ToString();
-                string firstName = reader["username"].ToString();
-                string lastName = reader["username"].ToString();
-                string email = reader["username"].ToString();
-                long role = (long)reader["role"];
-                string salt = reader["salt"].ToString();
+                if (reader.Read())
+                {
+                    long id = (long)reader["id"];
+                    string username = reader["username"].ToString();
+                    string firstName = reader["first_name"].ToString();
+                    string lastName = reader["last_name"].ToString();
+                    string email = reader["email"].ToString();
+                    long role = (long)reader["role"];
+                    DateTime joinedDate = (DateTime)reader["date_joined"];
 
-                user = new User(id, username, firstName, lastName, email, (UserRole) role);
+                    if (role != 2) return null;
 
+                    query = @"SELECT (experience_level) from developer WHERE user_id=@id;";
+
+                    _command = new SQLiteCommand(query, _connection);
+                    _command.Parameters.AddWithValue("@id", userId);
+                    reader = _command.ExecuteReader();
+
+                    if (reader.Read())
+                        return new Developer(id, username, firstName, lastName, email, "", joinedDate, (ExperienceLevelDeveloper)(long)reader["experience_level"]);
+
+
+                }
+                return null;
             }
 
-            Close();
 
-            return user;
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        internal static TechLeader GetTechLeaderById(long userId)
+        {
+            try
+            {
+                Open();
+
+                string query = @"SELECT id, username, email, first_name, last_name, date_joined, role
+                 FROM user
+                 WHERE id = @id;";
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@id", userId);
+
+                var reader = _command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    long id = (long)reader["id"];
+                    string username = reader["username"].ToString();
+                    string firstName = reader["first_name"].ToString();
+                    string lastName = reader["last_name"].ToString();
+                    string email = reader["email"].ToString();
+                    long role = (long)reader["role"];
+                    DateTime joinedDate = (DateTime)reader["date_joined"];
+
+                    if (role != 1) return null;
+
+                    query = @"SELECT (specialization) from techleader WHERE user_id=@id;";
+
+                    _command = new SQLiteCommand(query, _connection);
+                    _command.Parameters.AddWithValue("@id", userId);
+                    reader = _command.ExecuteReader();
+
+                    if (reader.Read())
+                        return new TechLeader(id, (Specializations)(long)reader["specialization"], username, firstName, lastName, email, "", joinedDate);
+
+                }
+                return null;
+            }
+
+
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                Close();
+            }
         }
 
         internal static long GetUserIdByEmail(string email)
         {
             long userId = 0;
-            Open();
+            try
+            {
+                Open();
 
-            string query = @"SELECT id 
+                string query = @"SELECT id 
                  FROM user
                  WHERE email = @email;";
-            _command = new SQLiteCommand(query, _connection);
-            _command.Parameters.AddWithValue("@email", email);
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@email", email);
 
-            var reader = _command.ExecuteReader();
+                var reader = _command.ExecuteReader();
 
-            if (reader.Read())
-                userId = (long)reader["id"];
+                if (reader.Read())
+                    userId = (long)reader["id"];
+                return userId;
 
-            Close();
+            }
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return userId;
+            }
+            finally { Close(); }
 
-            return userId;
+
         }
 
         internal static string GetUserFirstNameById(long userId)
@@ -290,5 +369,192 @@ namespace CodeFlowBackend.Repositories
 
             return userFirstName;
         }
+
+        internal static long GetUserRoleById(long userId)
+        {
+
+            long role = 0;
+            Open();
+
+            string query = @"SELECT role 
+                 FROM user
+                 WHERE id = @id;";
+            _command = new SQLiteCommand(query, _connection);
+            _command.Parameters.AddWithValue("@id", userId);
+
+            var reader = _command.ExecuteReader();
+
+            if (reader.Read())
+                role = (long)reader["role"];
+
+            Close();
+
+            return role;
+        }
+
+        internal static int GetUsersProjectCountById(long id)
+        {
+            try
+            {
+                Open();
+
+                string query = @"SELECT COUNT(project_id)
+                 FROM project_members
+                 WHERE member_id = @id;";
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@id", id);
+
+                var reader = _command.ExecuteReader();
+
+                if (reader.Read())
+                    return (int)(long)reader[0];
+                return 0;
+            }
+
+
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        internal static int GetUsersDoneTasksCountById(long id)
+        {
+            try
+            {
+                Open();
+
+                string query = @"SELECT COUNT(id)
+                 FROM assignment
+                 WHERE assignee = @id AND status = 3;";
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@id", id);
+
+                var reader = _command.ExecuteReader();
+
+                if (reader.Read())
+                    return (int)(long)reader[0];
+                return 0;
+            }
+
+
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        internal static bool UpdateTechLeader(long id, string firstName, string lastName, string email)
+        {
+            try
+            {
+                Open();
+
+                string query = @"UPDATE user
+                    SET first_name = @firstName,
+                        last_name = @lastName,
+                        email = @email
+                    WHERE id = @userId;";
+
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@firstName", firstName);
+                _command.Parameters.AddWithValue("@lastName", lastName);
+                _command.Parameters.AddWithValue("@email", email);
+                _command.Parameters.AddWithValue("@userId", id);
+                _command.ExecuteNonQuery();
+
+                return true;
+            }
+
+
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        internal static bool UpdateDeveloper(long id, string firstName, string lastName, string email, int experienceLevel)
+        {
+            try
+            {
+                if (!UpdateUser(id, firstName, lastName, email))
+                    return false;
+
+                Open();
+
+                string query = @"UPDATE developer
+                    SET experience_level = @experiencelevel
+                    WHERE user_id = @userId;
+                    ";
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@experiencelevel", experienceLevel);
+                _command.Parameters.AddWithValue("@userId", id);
+
+                _command.ExecuteNonQuery();
+                return true;
+            }
+
+
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static bool UpdateUser(long id, string firstName, string lastName, string email)
+        {
+
+            try
+            {
+                Open();
+
+                string query = @"UPDATE user
+                    SET first_name = @firstName,
+                        last_name = @lastName,
+                        email = @email
+                    WHERE id = @userId;
+                    ";
+                _command = new SQLiteCommand(query, _connection);
+                _command.Parameters.AddWithValue("@firstName", firstName);
+                _command.Parameters.AddWithValue("@lastName", lastName);
+                _command.Parameters.AddWithValue("@email", email);
+                _command.Parameters.AddWithValue("@userId", id);
+
+                _command.ExecuteNonQuery();
+                return true;
+            }
+
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        
     }
 }
