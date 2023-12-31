7d0f22ded452b7c038a527eb3f7b137c6d135765
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-31 17:01:23 -0300

    feat: enter project customized exception

---
+++ CodeFlowBackend/Exceptions/EnterProjectException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Exceptions
+{
+    public class EnterProjectException : Exception
+    {
+        public string Message { get; private set; }
+        public EnterProjectException(string message) { 
+            this.Message = message;
+        }
+    }
+}
