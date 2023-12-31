ff2bdfc80e8f0bacdfd05d42cc51dc318ff1fdf3
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-31 14:56:37 -0300

    feat: creating change password page

---
+++ CodeFlowUI/Pages/ChangePasswordPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeFlowUI.Pages
+{
+    public partial class ChangePasswordPage : Form
+    {
+        public ChangePasswordPage(long userId)
+        {
+            InitializeComponent(userId);
+        }
+    }
+}
