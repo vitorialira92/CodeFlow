9baaea4d438816e72854c7477ad964e4d87399ab
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-02 19:31:11 -0300

    feat: required dtos do load specific project page

---
+++ CodeFlowBackend/DTO/OpenTaskPageDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.DTO
+{
+    public class OpenTaskPageDTO
+    {
+        public long UserId { get; private set; }
+        public long TaskId { get; private set; }
+        public bool isUserTechLeader { get; private set; }
+
+        public OpenTaskPageDTO(long userId, long taskId, bool isUserTechLeader)
+        {
+            UserId = userId;
+            TaskId = taskId;
+            this.isUserTechLeader = isUserTechLeader;
+        }
+    }
+}
---
+++ CodeFlowBackend/DTO/TaskCardDTO.cs
@@ -0,0 +1,29 @@
+﻿using CodeFlowBackend.Model.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.DTO
+{
+    public class TaskCardDTO
+    {
+        public long Id { get; private set; }    
+        public string Name { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public string Assignee { get; private set; }
+        public (int done, int total) Checklist { get; private set; }
+        public TasksStatus Status { get; private set; } 
+
+        public TaskCardDTO(long id, string name, DateTime dueDate, string assignee, (int done, int total) checklist, TasksStatus status)
+        {
+            Id = id;
+            Name = name;
+            DueDate = dueDate;
+            Assignee = assignee;
+            Checklist = checklist;
+            Status = status;
+        }
+    }
+}
