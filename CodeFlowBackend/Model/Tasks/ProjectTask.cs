8468564584e9077db37ad86f7bda4ef7baa5ab6e
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-02 19:31:25 -0300

    feat: task entity

---
+++ CodeFlowBackend/Model/Tasks/ProjectTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Model.Tasks
+{
+    internal class ProjectTask
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public string Assignee {  get; private set; }
+        public TasksStatus Status { get; private set; }
+
+        public ProjectTask(long id, string name, string description, DateTime dueDate, string assignee, TasksStatus status)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+            Assignee = assignee;
+            Status = status;
+        }
+    }
+}
