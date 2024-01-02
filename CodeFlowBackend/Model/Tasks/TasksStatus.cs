4875cb173e68d18413d783278253b789a824ffc5
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-02 19:31:42 -0300

    feat: task status enum

---
+++ CodeFlowBackend/Model/Tasks/TasksStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Model.Tasks
+{
+    public enum TasksStatus
+    {
+        Todo = 1,
+        InProgress = 2,
+        Review = 3,
+        Done = 4
+    }
+}
