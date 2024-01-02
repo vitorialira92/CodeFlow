03cbe8257b9cdfb6b1f3feb72405f74c322b0625
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-02 19:32:48 -0300

    feat: project settings page file

---
+++ CodeFlowUI/Pages/ProjectSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeFlowUI.Pages
+{
+    public partial class ProjectSettings : Form
+    {
+        public ProjectSettings(long projectId)
+        {
+            InitializeComponent(projectId);
+        }
+    }
+}
