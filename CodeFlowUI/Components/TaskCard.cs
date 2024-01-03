1f63fd52af96bad23c0e8fc92f6a33985a407de1
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-03 15:18:00 -0300

    fix: checklist was missing

--- CodeFlowUI/Components/TaskCard.cs
+++ CodeFlowUI/Components/TaskCard.cs
@@ -8,32 +8,73 @@ using System.Text;
 using static System.Data.Entity.Infrastructure.Design.Executor;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using CodeFlowBackend.Model.Tasks;
+using System.Formats.Asn1;
 
 namespace CodeFlowUI.Components
 {
     internal class TaskCard : Panel
     {
-        public TaskCard(string taskName, DateTime dueDate,(int done, int total) checklist, string assignee, TasksStatus status)
+        public TaskCard(string taskName, DateTime dueDate,(int done, int total) checklist, string assignee, TasksStatus status, Tag? tag)
         {
             Width = 260;
             Height = 112;
             BorderStyle = BorderStyle.None;
             AddTaskName(taskName, status);
             AddDueDate(dueDate.Date);
-            AddChecklist();
-            if (dueDate < DateTime.Now)
+            AddChecklist(checklist);
+            if (dueDate < DateTime.Today)
                 AddLateLabel();
             AddAssignee(assignee);
+            if (tag != null)
+                AddTag(tag);
         }
 
-        private void AddChecklist()
+        private void AddTag(Tag tag)
         {
-            PictureBox checklist = new PictureBox();
-            checklist.Image = Image.FromFile(@"Resources\checklist.png");
-            checklist.Size = new Size(20,20);
-            checklist.Location = new Point(20, 61);
+            Panel statusCircle = new Panel();
+            statusCircle.Size = new Size(9, 9);
+            statusCircle.BackColor = Colors.ProjectCardBackgroundColor;
 
-            Controls.Add(checklist);
+            statusCircle.Paint += (sender, e) =>
+            {
+                SolidBrush brush = new SolidBrush(tag.Color);
+
+                e.Graphics.FillEllipse(brush, 0, 0, statusCircle.Width, statusCircle.Height);
+
+            };
+            statusCircle.Location = new Point(175, 92);
+
+            Controls.Add(statusCircle);
+
+            Label tagLabel = new Label();
+            tagLabel.Text = tag.Name;
+            tagLabel.ForeColor = Colors.DarkBlue;
+            tagLabel.Font = new Font("Ubuntu Light", 8);
+            tagLabel.Location = new Point(191, 89);
+            tagLabel.AutoSize = true;
+            tagLabel.BackColor = Color.Transparent;
+            Controls.Add(tagLabel);
+        }
+
+        private void AddChecklist((int done, int total) checklist)
+        {
+            PictureBox checklistPicture = new PictureBox();
+            checklistPicture.Image = Image.FromFile(@"Resources\checklist.png");
+            checklistPicture.Size = new Size(20,20);
+            checklistPicture.Location = new Point(20, 61);
+
+            Controls.Add(checklistPicture);
+
+            Label statsLabel = new Label();
+            statsLabel.Text = $"{checklist.done}/{checklist.done}";
+            statsLabel.ForeColor = Colors.DarkBlue;
+            statsLabel.AutoSize = true;
+            statsLabel.Location = new Point(53,61);
+            statsLabel.BackColor = Color.Transparent;
+            statsLabel.Font = new Font("Ubuntu Light", 8);
+
+            Controls.Add(statsLabel);
+
         }
     
 
@@ -51,14 +92,14 @@ namespace CodeFlowUI.Components
 
         private void AddLateLabel()
         {
-            Label dueDateLabel = new Label();
-            dueDateLabel.Text = "late";
-            dueDateLabel.ForeColor = Colors.ErrorColor;
-            dueDateLabel.Font = new Font("Ubuntu Light", 8);
-            dueDateLabel.Location = new Point(219, 89);
-            dueDateLabel.AutoSize = true;
-            dueDateLabel.BackColor = Color.Transparent;
-            Controls.Add(dueDateLabel);
+            Label lateLabel = new Label();
+            lateLabel.Text = "late";
+            lateLabel.ForeColor = Colors.ErrorColor;
+            lateLabel.Font = new Font("Ubuntu Light", 8);
+            lateLabel.Location = new Point(222, 39);
+            lateLabel.AutoSize = true;
+            lateLabel.BackColor = Color.Transparent;
+            Controls.Add(lateLabel);
         }
 
         private void AddStatus(TasksStatus status)
