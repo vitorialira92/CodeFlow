a9b90c8a0ec845919621c376f0edf19a2cd2ce20
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-03 19:50:41 -0300

    feat: create project dto

---
+++ CodeFlowBackend/DTO/CreateProjectDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.DTO
+{
+    public class CreateProjectDTO
+    {
+        public long TechLeaderId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DueDate { get; private set; }
+
+        public CreateProjectDTO(long techLeaderId, string name, string description, DateTime dueDate)
+        {
+            TechLeaderId = techLeaderId;
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+        }
+    }
+}
