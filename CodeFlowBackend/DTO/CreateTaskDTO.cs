c2123def895975d178a006568f2208c2a88cdb77
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-03 15:17:06 -0300

    feat: create task dto

---
+++ CodeFlowBackend/DTO/CreateTaskDTO.cs
@@ -0,0 +1,31 @@
+﻿using CodeFlowBackend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.DTO
+{
+    public class CreateTaskDTO
+    {
+        public long ProjectId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Tag tag { get; private set; }
+        public List<string> Checklist { get; private set; }
+        public string AssigneeUsername { get; private set; }
+        public DateTime DueDate { get;private set; }
+
+        public CreateTaskDTO(long projectId, string name, string description, Tag? tag, List<string>? checklist, string assigneeUsername, DateTime dueDate)
+        {
+            ProjectId = projectId;
+            Name = name;
+            Description = description;
+            this.tag = tag;
+            Checklist = checklist;
+            AssigneeUsername = assigneeUsername;
+            DueDate = dueDate;
+        }
+    }
+}
