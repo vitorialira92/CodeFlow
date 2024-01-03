0c993b57b9e44512c828ddcc2be416f15bbca863
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-03 15:17:49 -0300

    feat: tag

---
+++ CodeFlowBackend/Model/Tag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowBackend.Model
+{
+    public class Tag
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public Color Color {  get; private set; }
+
+        public Tag(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public Tag(long id, string name, Color color)
+        {
+            Id = id;
+            Name = name;
+            Color = color;
+        }
+    }
+}
